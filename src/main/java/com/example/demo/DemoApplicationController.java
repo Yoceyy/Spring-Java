0b95f8ba8f65c69017265736eebea1b34965aab4
Yoceyy <mohamed.blanche@yahoo.fr>
2023-06-05 09:31:46 +0200

    added DemoApplication

--- src/main/java/com/example/demo/DemoApplicationController.java
+++ src/main/java/com/example/demo/DemoApplicationController.java
@@ -5,13 +5,13 @@ package com.example.demo;
 import java.io.IOException;
 import java.io.InputStream;
 
-import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.http.MediaType;
 
 import org.apache.tomcat.util.http.fileupload.IOUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,8 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoApplicationController {
 
-    @Autowired
-    private ServletContext servletContext;
+
 
     @GetMapping("/")
     public String hello() {
@@ -42,13 +41,12 @@ public class DemoApplicationController {
 
 
 
-    @RequestMapping(value = "/image/", method = RequestMethod.GET, produces = MediaType.IMAGE_JPEG_VALUE )
-
-    public void getImageAsByteArray(HttpServletResponse response) throws IOException  {
-        InputStream in = servletContext.getResourceAsStream("/image/image2.jpg");
+    @RequestMapping(value="/image", method = RequestMethod.GET, produces = MediaType.IMAGE_JPEG_VALUE)
+    public void getImageAsByteArray(HttpServletRequest request, HttpServletResponse response) throws IOException  {
+        ClassPathResource resource = new ClassPathResource("/image/image2.jpg");
+        InputStream in = resource.getInputStream();
         IOUtils.copy(in, response.getOutputStream());
     }
-
 
 
 
